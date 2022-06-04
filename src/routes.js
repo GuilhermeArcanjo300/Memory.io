a729d07dd5bf9492ad60cf44ef8699a8cee46933
Guilherme Arcanjo <95060340+GuilhermeArcanj0@users.noreply.github.com>
2022-06-04 11:50:58 -0300

    adding timer, change with animation of card and verification

--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,26 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render(path.join(__dirname, 'views', 'index.html'));
+  res.render(path.join(__dirname, 'views', 'index.handlebars'), {layout: false});
 });
 
 router.get('/game', (req, res) => {
-  res.render(path.join(__dirname, 'views', 'board.html'));
+
+  const cards = [
+    {value: 1, title: 'card1'}, 
+    {value: 1, title: 'card2'}, 
+    {value: 2, title: 'card3'}, 
+    {value: 2, title: 'card4'}, 
+    {value: 3, title: 'card5'}, 
+    {value: 3, title: 'card6'}
+  ]
+
+  const data = {
+    layout: false,
+    cards: cards
+  }
+
+  res.render(path.join(__dirname, 'views', 'board.handlebars'), data);
 });
 
 module.exports = router;
