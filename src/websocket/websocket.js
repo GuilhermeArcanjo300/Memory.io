4ae6fe8532eb60b7e933f88c3e86ab5c2f2421b2
Murilo Manente <munente2002@gmail.com>
2022-06-12 22:25:04 -0300

    Creating a websocket instance, player login and validation if players is ready

--- src/websocket/websocket.js
+++ src/websocket/websocket.js
@@ -1,3 +1,35 @@
 const { io } = require('../../config/http');
 
-io.on('connection', (socket) => console.log(socket.id));
+const players = [];
+
+io.on('connection', (socket) => {
+    // #region player
+    const player = {
+        id: socket.id,
+        nickname: null,
+    };
+
+    players.push(player);
+
+    socket.emit('playersJoined', players.length);
+
+    socket.on('sendNickname', (nicknamePlayer) => {
+        players[players.length - 1].nickname = nicknamePlayer;
+
+        const allNicknames = players.map((index) => index.nickname);
+
+        io.emit('setNicknamesInLabels', allNicknames);
+    });
+
+    socket.on('removeLastPlayer', () => {
+        console.log(`Removing last socket: ${players[players.length - 1].id}`);
+        players.pop();
+    });
+    // #endregion players
+
+    if (players.length === 2) io.emit('checkIfPlayersReady');
+
+    socket.on('playerReady', () => {
+        console.log(socket.id);
+    });
+});
