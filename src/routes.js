5aedfe89d2c33d086854c42d49f79f583f38c5b7
Guilherme Arcanjo <95060340+GuilhermeArcanj0@users.noreply.github.com>
2022-06-26 15:24:10 -0300

    resolved eslint

--- src/routes.js
+++ src/routes.js
@@ -1,26 +1,24 @@
 const path = require('path');
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.render(path.join(__dirname, 'views', 'index.handlebars'), {layout: false});
+  res.render(path.join(__dirname, 'views', 'index.handlebars'), { layout: false });
 });
 
 router.get('/game', (req, res) => {
-
   const cards = [
-    {value: 1, title: 'card1'}, 
-    {value: 1, title: 'card2'}, 
-    {value: 2, title: 'card3'}, 
-    {value: 2, title: 'card4'}, 
-    {value: 3, title: 'card5'}, 
-    {value: 3, title: 'card6'}
-  ]
+    { value: 1, img: '01.png' },
+    { value: 1, img: '01.png' },
+    { value: 2, img: '02.jpg' },
+    { value: 2, img: '02.jpg' },
+    { value: 3, img: '03.jpg' },
+    { value: 3, img: '03.jpg' },
+  ];
 
   const data = {
     layout: false,
-    cards: cards
-  }
+    cards,
+  };
 
   res.render(path.join(__dirname, 'views', 'board.handlebars'), data);
 });
